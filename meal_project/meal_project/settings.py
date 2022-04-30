1224d773e0b529a5ae24a2818816775b2b28bd8b
root <pafend>
2022-04-30 22:32:50 +0000

    fix typo BASE*D*_DIR

--- meal_project/meal_project/settings.py
+++ meal_project/meal_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'meal_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASED_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
